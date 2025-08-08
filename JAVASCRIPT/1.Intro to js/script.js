21d902aec9d55cd2d27ccdb93330587840793ed1
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-08 19:28:52 +0530

    Add javaScript File

---
+++ JAVASCRIPT/1.Intro to js/script.js
@@ -0,0 +1,35 @@
+// let name = "Prince";
+// let age = 22;
+// let isEligibleToVote = true;
+
+// console.log("My Name is :" + name);
+// console.log("My age is ", age);
+// console.log("is eligible to vote", isEligibleToVote);
+
+// let a = 12;
+// let b = 10;
+// let sum = a + b;
+// //Without third variable using
+// console.log(a + b);
+// console.log(a + b - 5);
+// //using third variable
+// console.log(sum);
+// console.log(sum - 5);
+
+let varName = "Prince";
+// varName = "Intro to js";
+// varName = 22;
+console.log(varName);
+
+const office = "ABC";
+console.log(office);
+
+// office = "XYZ";
+
+var colloge = "NIT";
+colloge = "IIT";
+console.log(colloge);
+
+
+let varName1;   //Declaration
+varName1 ="Yadav" // Initilazation
---
+++ JAVASCRIPT/2.Data Types in js/sciprt.js
@@ -0,0 +1,30 @@
+// let a = 123;
+// a = true;
+// let b = 9;
+
+// let sum = null;
+// sum = a + b;
+// console.log("The value of a is : ", a);
+// console.log("The value of b", b);
+// console.log("The type of a is:", typeof a);
+// console.log("The type of b ", typeof b);
+
+// console.log(sum);
+
+// let v1 = 12;
+// console.log(v1 + 1);
+
+// let v2 = "12";
+// console.log(v2 + 1);  // appends like output 121
+
+let abc = 124;
+let result1 = String(abc);
+console.log("The value of abc is ", abc);
+console.log("The type of abc is ", typeof abc);
+console.log("The type of abc is ", typeof result1);
+
+let name = "prince";
+name = Number(name);
+
+console.log("The value of name is", name); //NaN = Not a Number
+console.log("The type of name is", typeof name);
