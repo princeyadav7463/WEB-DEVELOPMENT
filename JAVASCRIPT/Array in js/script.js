56ad5063ba8404cd35b25bd23ce3bd2b640bd772
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-10 07:37:41 +0530

    add Array files

---
+++ JAVASCRIPT/Array in js/script.js
@@ -0,0 +1,59 @@
+let varName = 123;
+
+let arrName = [123, 245, 895];
+
+let std1 = "Prince";
+let std2 = "Mamta";
+let atd3 = "Aditya";
+
+let studentsArray = [
+  "Prince",
+  "Dheeraj",
+  "Abhishek",
+  "Yogendra",
+  "Raju",
+  true,
+  123,
+];
+//  index num            0,       1,         2,         3,        4
+
+// console.log(studentsArray);
+// console.log(studentsArray[6]);
+
+// console.log(studentsArray.length);
+
+// studentsArray[2] = "28";
+
+// console.log(studentsArray);
+
+// let newArr = studentsArray;
+// console.log(newArr);
+// newArr[0] = "Students";
+// console.log(newArr);
+// console.log(studentsArray);
+
+// console.log(studentsArray.indexOf("xyz"));
+// console.log(studentsArray.includes(456));
+// console.log(studentsArray);
+// studentsArray.push("Laptop");
+// console.log(studentsArray);
+// studentsArray.unshift("Mobile");
+// console.log(studentsArray);
+
+// console.log(studentsArray);
+// studentsArray.pop();
+// console.log(studentsArray);
+
+// console.log(studentsArray);
+// studentsArray.shift();
+// console.log(studentsArray);
+
+// let marks = [98, 78, 65, 34, 33, 95, 125];
+// //  index num: 0, 1, 2,   3,  4, 5,  6
+// console.log(marks);
+
+// marks.sort();
+// console.log(marks);
+
+// let subArrMarks = marks.slice(2, 6);
+// console.log(subArrMarks);
