7ed4d0481c994d2863c74708f3938097ff5c7cd1
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-10 15:36:49 +0530

    Advanced function

---
+++ JAVASCRIPT/9.Advanced Functions in js/script.js
@@ -0,0 +1,67 @@
+// //  Named Functions
+
+// function greet() {
+//   console.log("Hey everyone!");
+// }
+
+// greet();
+
+// // Anonymous Function or function expression
+
+// let greet2 = function () {
+//   console.log("Hey everyone!");
+// };
+
+// greet2();
+
+// // arrow Function
+// let greet3 = () => {
+//   console.log("Hey everyone!");
+// };
+
+// greet3();
+
+// const greet = (count) => {
+//   console.log("Hello all", count);
+// };
+
+// greet(2);
+
+// const square = () => {
+//   return num * num;
+// };
+// console.log(square(2));
+
+// Callback Funs
+// const calculate = ( a , b, operation) =>{
+//   return operation(a,b)
+// }
+
+// console.log(calculate(2,3,funcation(n1,n2){
+//   return n1+n2;
+// ))}
+
+// // function definition
+// function sayMyName() {
+//   console.log("Prince Yadav");
+// }
+
+// // Function call
+
+// sayMyName();
+
+// let getMultiplication = function (a, b) {
+//   return a * b;
+// };
+
+// let ans = getMultiplication(2, 20);
+// console.log(ans);
+
+// arrow Function
+
+let getExp = (x, y) => {
+  let ans = x ** y;
+  return ans;
+};
+console.log(getExp(2, 10));
+
