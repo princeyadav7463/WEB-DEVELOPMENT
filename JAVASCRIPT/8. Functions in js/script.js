0152fe75846f4bed865595dbd652ccf3c32e0db6
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-10 14:58:10 +0530

    Add Function file

---
+++ JAVASCRIPT/8. Functions in js/script.js
@@ -0,0 +1,72 @@
+//
+// some other code snippet
+//
+// const a = 10;
+// const b = 2;
+
+// const aPlusB = a + b;
+// console.log("Result:", aPlusB);
+
+// sumation(a, b);
+
+// //
+// //  some other code sinppet
+// //
+
+// const x = 12;
+// const y = 7;
+// const xPlusY = x + y;
+// console.log("Result:", xPlusY);
+// sumation(x, y);
+
+// //
+// // Some other code snippet
+// //
+
+// const l = 20;
+// const k = 4;
+// const lPlusK = l + k;
+// console.log("Result:", lPlusK);
+// sumation(l, k);
+//  //func def
+
+// sumation(22, 32);
+
+// function sumation(r, k) {
+//   const result = r + k;
+//   console.log("Result:", result);
+// }
+
+// // func call
+// sumation(20, 30);
+
+// function greet() {
+//   console.log("Hey Welcome to Our Applications");
+// }
+
+// greet();
+
+// function greet(userName) {
+//   console.log("Hey welcome to our applications", userName);
+// }
+
+// greet("Prince Yadav");
+
+// // anonymous function Expression
+
+// let sumation = function (r, k) {
+//   const result = r + k;
+//   return result;
+// };
+// console.log(sumation(20, 30));
+
+// Nested function
+function addSquares(x, y) {
+  function square(num) {
+    return num * num;
+  }
+  console.log(square(4));
+
+
+}
+addSquares(1, 2);
