537bb23cc9e13c8eaae0d652cda5cbf64b676e77
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-11 20:45:35 +0530

    add

---
+++ JAVASCRIPT/12.DOM manipulation in js/script.js
@@ -0,0 +1,54 @@
+// console.log(document.body);
+
+const bodyBackground = document.body;
+// bodyBackground.style.backgroundColor = "green";
+
+//  1st approach
+
+// const box4 = document.getElementById("box-4");
+// console.log(box4);
+
+// 2nd approach
+// const div = document.getElementById("div");
+// console.log(div);
+
+// 3rd approach
+// const boxes = document.getElementByIdClassName("box");
+// console.log(boxes);
+
+// Modifying html Elements
+
+// const box1 = document.getElementById("box-1");
+// box1.innerHTML = "<h1>Prince Yadav</h1>";
+// box1.innerText = "Prince"
+
+// document.getElementById("box-1").innerText = "Prince Yadav";
+
+// document.getElementsByTagName("p").innerText = "Prince Yadav From Jharkhand";
+
+// const box1 = document.getElementById("box-1");
+// box1.style.backgroundColor = "yellow";
+
+// const box2 = document.getElementById("box-2");
+// box2.classList.add("circle");
+
+// const box3 = document.getElementById("box-3");
+// box3.classList.remove("circle");
+
+// const boxes = document.getElementsByClassName("box");
+// for (let i = 0; i < boxes.length; t++) {
+//   boxes[i].classList.add("circle");
+// }
+// box3.classList.remove("box");
+
+//  1st is to get the access
+// end is the contents
+// 3rd is appending
+
+// const newPara = document.createElement("p");
+// newPara.innerText = "This is our new paragraph";
+// const cont = document.getElementById("cont");
+// cont.appendChild(newPara);
+
+// newPara.classList.add("box");
+// newPara.classList.add("circle");
---
+++ JAVASCRIPT/13.Event Handling in js/script.js
@@ -0,0 +1,8 @@
+// Events
+
+// 1. Event Handler
+// (onclick, onload, onmouseover, onmouseout, onkeypress, onkeydown, onkeyup)
+
+function onclickFun() {
+  console.log("This box has been clicked");
+}
