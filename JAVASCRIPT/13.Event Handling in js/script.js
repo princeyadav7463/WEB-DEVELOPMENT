76dacbdbd15fef1ecc2c96a04f555a612383eb3d
princeyadav7463 <princeyadav875791@gmail.com>
2025-08-17 16:01:46 +0530

    update

--- JAVASCRIPT/13.Event Handling in js/script.js
+++ JAVASCRIPT/13.Event Handling in js/script.js
@@ -3,7 +3,47 @@
 // 1. Event Handler
 // (onclick, onload, onmouseover, onmouseout, onkeypress, onkeydown, onkeyup)
 
-function onclickFun() {
-  console.log("This box has been clicked");
-}
-console.log("Prince Yadav");
+// function onclickFun() {
+//   console.log("This box has been clicked");
+// }
+
+// const keyPressEvent = () => {
+//   console.log("The keep has been hitted");
+// };
+
+// const onkeyup = () => {
+//   console.log("onkeyup ");
+// };
+
+// const onkeydown = () => {
+//   console.log("onkeydown");
+// };
+
+// function firstFuncCall() {
+//   console.log("func 1");
+// }
+
+// function secondFuncCall() {
+//   console.log("Sec  2");
+// }
+
+// 2. Event Listerns
+const box1 = document.getElementById("box-1");
+box1.addEventListener("click", () => {
+  // console.log("Click has been triggered");
+  console.log("Box");
+});
+
+// box1.addEventListener("click", () => {
+//   console.log("Click has been triggered");
+// });
+
+const cont = document.getElementById("cont");
+box1.addEventListener(
+  "click",
+  () => {
+    // console.log("Click has been triggered");
+    console.log("Container");
+  },
+  true
+);
---
+++ TAILWINDS CSS/1.Intro/introToTailwind.md
@@ -0,0 +1,29 @@
+## css Framework
+
+## Single utility
+
+## Almost no need to write different css
+
+## A-lot of varilable classes for style
+
+## No need to overide css
+
+## Fast development
+
+## Fast Performance
+
+## Light weight Framewok
+
+## setup (CDN, with node(cli)) CDN:- Content Deliver Network.
+
+# Basics
+
+# Basic page Development
+
+# Customizing Tailwind
+
+# Responsive UI
+
+# Task & Project
+
+# Interview questions
