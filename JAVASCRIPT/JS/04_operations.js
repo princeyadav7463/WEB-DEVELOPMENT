4ab32f49ae93fbd637b795d436db302459eeacfc
princeyadav7463 <princeyadav875791@gmail.com>
2025-09-11 08:45:26 +0530

    Operation

---
+++ JAVASCRIPT/JS/04_operations.js
@@ -0,0 +1,26 @@
+// Operations
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello";
+let str2 = " Prince";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
