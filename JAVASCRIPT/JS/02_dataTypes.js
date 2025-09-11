85ce06339ee2909b4c92b67e18c445b81a1b129c
princeyadav7463 <princeyadav875791@gmail.com>
2025-09-11 08:14:12 +0530

    datatypes

---
+++ JAVASCRIPT/JS/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as nwer version
+
+// alert("hello");  // we are using nodejs, not browser
+
+console.log("Hello");
+
+let name = "Prince";
+
+let age = 22;
+
+let isLoggedIn = false;
+
+// number
+// string
+// boolean
+// null
+// undefinded
